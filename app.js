18adf26a3c6f68ca687803560d709cfca79cbbbc
neev0511 <allofbakwas@gmail.com>
2021-06-08 16:02:58 +0530

    Made Many changes

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const navbar = document.querySelector('.navbar');
 const navlinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
+const about = document.querySelector('#About');
+const drop = document.querySelector('#Drop');
+const contact = document.querySelector('#Contact');
+const drop2 = document.querySelector('#Drop2');
 
 navbar.addEventListener("click", () => {
   navlinks.classList.toggle("open");
+})
+about.addEventListener("click", () => {
+  drop.classList.toggle("open");
+})
+contact.addEventListener("click", () => {
+  drop2.classList.toggle("open");
 })
